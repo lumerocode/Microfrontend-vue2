ea902d276458c3ff9f1c80381f1eca9a293c8112
lumerocode <luismelendez0894@gmail.com>
2024-04-16 10:01:30 -0500

    Microfrontend vue2

---
+++ vite.config.js
@@ -0,0 +1,49 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue2 from '@vitejs/plugin-vue2'
+import federation from '@originjs/vite-plugin-federation'
+import topLevelAwait from 'vite-plugin-top-level-await'
+
+const APPLICATION_PORT = 5005
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    assetsInlineLimit: 40960,
+    minify: true,
+    cssCodeSplit: false,
+    sourcemap: true,
+  },
+  server: {
+    port: APPLICATION_PORT
+  },
+  preview: {
+    port: APPLICATION_PORT
+  },
+  plugins: [
+    vue2(),
+    federation({
+      name: 'Microfrontend-vue2',
+      filename: 'Microfrontend-vue2.js',
+      // Modules to expose
+      exposes: {
+        './Microfrontend-vue2': './src/components/HelloWorld.vue',
+      },
+      shared: {
+        vue: {},
+      }
+    }),
+    topLevelAwait({
+      // The export name of top-level await promise for each chunk module
+      promiseExportName: "__tla",
+      // The function to generate import names of top-level await promise in each chunk module
+      promiseImportName: i => `__tla_${i}`
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
